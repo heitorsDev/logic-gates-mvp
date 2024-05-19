af1acda2e7c4a219f3ea2dfe5bc68c197814e26b
unknown <heitorhilles@gmail.com>
2024-05-19 13:53:16 -0300

    segundo commit (eu nao aguento mais)

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,18 @@ function App() {
 
   return (
     <>
+      <div className="descriptions">
+        <div className="description-text">
+        Dois interruptores são como botões que podem ligar ou desligar uma luz. Para acender a luz, ambos os interruptores precisam estar ligados ao mesmo tempo. É como se fosse necessário apertar dois botões ao mesmo tempo para fazer algo acontecer. Quando um ou nenhum dos botões está pressionado, a luz fica apagada, mas quando os dois estão pressionados, a luz acende,        </div>
+        <a href="http://gg.gg/1au9oi"><div className="botao">
+          <b>Voltar</b>
+        </div></a>
+      </div>
     <div className="bg">
       <Circuit>
         
       </Circuit>
+
       </div>
     </>
   )
